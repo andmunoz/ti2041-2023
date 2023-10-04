ec16095362dc89a30c2e96ac97f0ef49b027a963
Andrés Muñoz Órdenes <andmunoz@gmail.com>
2023-10-04 17:18:12 -0300

    simple calc version

--- evaluaciones/sumativa1/calculadora/miapp/views.py
+++ evaluaciones/sumativa1/calculadora/miapp/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render, HttpResponse
 
-# Create your views here.
-def index(request):
-    return HttpResponse('Hola mundo')
+# Se llama con un valor para la variable page por default, así diferenciamos la vista final.
+def index(request, page="index.html"):
+    # Se define un contexto vacío
+    context = {}
+
+    # Se verifica el envío del formulario (independiente de la vista)
+    if request.POST:
+        page = request.POST['page']
+        context['operador'] = request.POST['operador']
+        
+        # Caso de operación binaria, se obtienen los operandos
+        context['operando1'] = float(request.POST['operando1'])
+        context['operando2'] = float(request.POST['operando2'])
+
+        # Se operan los operandos
+        match context['operador']:
+            case "adicion":
+                context['resultado'] = context['operando1'] + context['operando2']
+            case "sustraccion":
+                context['resultado'] = context['operando1'] - context['operando2']
+            case "multiplicacion":
+                context['resultado'] = context['operando1'] * context['operando2']
+            case "division":
+                if context['operando2'] == 0:
+                    context['resultado'] = 'Error: División por cero'
+                else: 
+                    context['resultado'] = context['operando1'] / context['operando2']
+            case _:
+                context['resultado'] = 'Error: Operación desconocida'
+
+    # Se renderiza la vista para mostrar el resultado
+    return render(request, page, context)
