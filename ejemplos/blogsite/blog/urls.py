db511de02d81173ff06e0431feef2d13d66f6a20
Andrés Muñoz O <andmunoz@moit.cl>
2023-11-14 00:06:04 -0300

    remove sumativa2 solution

--- ejemplos/blogsite/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('post/<post_id>/', views.index),
-    path('category/<category_id>/', views.index),
-    path('hashtag/<hashtag_id>/', views.index),
-]
