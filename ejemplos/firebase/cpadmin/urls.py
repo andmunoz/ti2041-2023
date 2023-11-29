3fbfad456613678261e8ef8e2a628df076873cd8
Andrés Muñoz O <andmunoz@moit.cl>
2023-11-29 17:29:36 -0300

    populate firebase action

--- ejemplos/firebase/cpadmin/urls.py
+++ ejemplos/firebase/cpadmin/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
 
     path('equipment/', views.equipment_home, name='equipment'),
     path('equipment/weapons', views.weapons_list, name='weapons'),
-    path('equipment/weapons/save', views.weapons_save),
+    path('equipment/weapons/create', views.weapons_create),
+    path('equipment/weapons/update', views.weapons_update),
+    path('equipment/weapons/delete', views.weapons_delete),
     path('equipment/weapons/download', views.weapons_download),
     path('equipment/weapons/upload', views.weapons_upload),
     path('equipment/weapons/refresh', views.weapons_refresh),
