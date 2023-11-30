3f95161f84055b441d2874dbb46b462a54764e07
Andrés Muñoz O <andmunoz@moit.cl>
2023-11-30 10:51:19 -0300

    add image field to weapon model

--- ejemplos/firebase/cpadmin/models.py
+++ ejemplos/firebase/cpadmin/models.py
@@ -56,6 +56,7 @@ class Weapon(models.Model):
     damage = models.CharField(max_length=20)
     weight = models.FloatField()
     cost = models.IntegerField()
+    image = models.CharField(max_length=255, null=True)
     
     def __str__(self):
         return self.name
