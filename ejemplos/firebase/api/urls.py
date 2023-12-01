8c233ec61ea5425f007b167da27b30f6a15b08fe
Andrés Muñoz O <andmunoz@moit.cl>
2023-12-01 00:48:58 -0300

    Remove Cyberpunnk Help Page Project

--- ejemplos/firebase/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .api import api
-
-urlpatterns = [
-    path("v1/", api.urls),
-]
--- ejemplos/firebase/cpadmin/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Category, Brand, Weapon
-
-admin.site.register(Category)
-admin.site.register(Brand)
-admin.site.register(Weapon)
